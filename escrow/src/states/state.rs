2151c212a533463f8dd71bac746492be5f0a1194
jeevan4476 <jeevanr2431@gmail.com>
2025-06-11 21:37:32 +0530

    Completed every instructions

--- escrow/src/states/state.rs
+++ escrow/src/states/state.rs
@@ -92,4 +92,213 @@ impl Escrow {
 
         Ok(())
     }
+
+    #[inline]
+    pub fn deposit<'a>(
+        escrow_address: &Pubkey,
+        token_program: &Pubkey,
+        amount: u64,
+        maker_ta_a: &AccountInfo<'a>,
+        mint_a: &AccountInfo<'a>,
+        vault: &AccountInfo<'a>,
+        maker: &AccountInfo<'a>,
+    ) -> ProgramResult {
+        //check PDA of vault
+        check_eq_PDA_and_get_bump(&[b"vault", escrow_address.as_ref()], &crate::ID, vault.key)?;
+        assert_eq!(
+            *escrow_address,
+            *<spl_token::state::Account as spl_token::state::GenericTokenAccount>::unpack_account_owner(*vault.try_borrow_data()?).ok_or(ProgramError::InvalidAccountData)?
+        );
+
+        let decimals = spl_token::state::Mint::unpack(&mint_a.try_borrow_data()?)?.decimals;
+        let ix = transfer_checked(
+            token_program,
+            maker_ta_a.key,
+            mint_a.key,
+            vault.key,
+            maker.key,
+            &[],
+            amount,
+            decimals,
+        )?;
+        let account_info = vec![
+            maker_ta_a.clone(),
+            mint_a.clone(),
+            vault.clone(),
+            maker.clone(),
+        ];
+        invoke(&ix, &account_info)?;
+        Ok(())
+    }
+
+    #[inline]
+    pub fn take<'a>(
+        escrow_data: Escrow,
+        bump: u8,
+        token_program: &Pubkey,
+        mint_a: &AccountInfo<'a>,
+        mint_b: &AccountInfo<'a>,
+        vault: &AccountInfo<'a>,
+        maker: &AccountInfo<'a>,
+        taker: &AccountInfo<'a>,
+        escrow: &AccountInfo<'a>,
+        maker_ta_b: &AccountInfo<'a>,
+        taker_ta_a: &AccountInfo<'a>,
+        taker_ta_b: &AccountInfo<'a>,
+    ) -> ProgramResult {
+        check_eq_PDA(&[b"vault", escrow.key.as_ref()], &crate::ID, vault.key)?;
+        assert_eq!(mint_a.key, &escrow_data.mint_a);
+        assert_eq!(mint_b.key, &escrow_data.mint_b);
+
+        // Get token decimals
+        let decimals_a = spl_token::state::Mint::unpack(&mint_a.try_borrow_data()?)?.decimals;
+        let decimals_b = spl_token::state::Mint::unpack(&mint_b.try_borrow_data()?)?.decimals;
+
+        // Get token amount
+        let amount = spl_token::state::Account::unpack(&vault.try_borrow_data()?)?.amount;
+
+        assert!([&spl_token::ID, &spl_token_2022::ID].contains(&token_program));
+
+        //claim token A to taker
+        let ix_1 = transfer_checked(
+            token_program,
+            vault.key,
+            mint_a.key,
+            taker_ta_a.key,
+            escrow.key,
+            &[],
+            amount,
+            decimals_a,
+        )?;
+
+        invoke_signed(
+            &ix_1,
+            &[
+                vault.clone(),
+                mint_a.clone(),
+                taker_ta_a.clone(),
+                escrow.clone(),
+            ],
+            &[&[
+                b"escrow",
+                maker.key.as_ref(),
+                escrow_data.seed.to_le_bytes().as_ref(),
+                &[bump],
+            ]],
+        )?;
+
+        //transfer token B to maker
+
+        invoke(
+            &transfer_checked(
+                token_program,
+                taker_ta_b.key,
+                mint_b.key,
+                maker_ta_b.key,
+                taker.key,
+                &[],
+                escrow_data.receive,
+                decimals_b,
+            )?,
+            &[
+                taker_ta_b.clone(),
+                mint_b.clone(),
+                maker_ta_b.clone(),
+                taker.clone(),
+            ],
+        )?;
+
+        //close the vault
+        invoke_signed(
+            &close_account(token_program, vault.key, maker.key, escrow.key, &[])?,
+            &[vault.clone(), maker.clone(), escrow.clone()],
+            &[&[
+                b"escrow",
+                maker.key.as_ref(),
+                escrow_data.seed.to_le_bytes().as_ref(),
+                &[bump],
+            ]],
+        )?;
+
+        // Close the escrow
+        let balance = escrow.lamports();
+        escrow.realloc(0, false)?;
+        **escrow.lamports.borrow_mut() = 0;
+        **maker.lamports.borrow_mut() += balance;
+        escrow.assign(&Pubkey::default());
+
+        Ok(())
+    }
+
+    #[inline]
+    pub fn refund<'a>(
+        escrow_data: Escrow,
+        bump: u8,
+        token_program: &Pubkey,
+        maker_ta_a: &AccountInfo<'a>,
+        mint_a: &AccountInfo<'a>,
+        escrow: &AccountInfo<'a>,
+        vault: &AccountInfo<'a>,
+        maker: &AccountInfo<'a>,
+    ) -> ProgramResult {
+        // Check PDA of vault
+        check_eq_PDA(&[b"vault", escrow.key.as_ref()], &crate::ID, vault.key)?;
+
+        // Check mints match
+        assert_eq!(mint_a.key, &escrow_data.mint_a);
+
+        // Get token decimals
+        let decimals = spl_token::state::Mint::unpack(&mint_a.try_borrow_data()?)?.decimals;
+
+        // Get token amount
+        let amount = spl_token::state::Account::unpack(&vault.try_borrow_data()?)?.amount;
+        // By checking this, we know our token accounts are correct by virtue of Token Program checking them
+        assert!([&spl_token::ID, &spl_token_2022::ID].contains(&token_program));
+
+        invoke_signed(
+            &transfer_checked(
+                token_program,
+                vault.key,
+                mint_a.key,
+                maker_ta_a.key,
+                escrow.key,
+                &[],
+                amount,
+                decimals,
+            )?,
+            &[
+                vault.clone(),
+                mint_a.clone(),
+                maker_ta_a.clone(),
+                escrow.clone(),
+            ],
+            &[&[
+                b"escrow",
+                maker.key.as_ref(),
+                escrow_data.seed.to_le_bytes().as_ref(),
+                &[bump],
+            ]],
+        )?;
+
+        //close the vault
+        invoke_signed(
+            &close_account(token_program, vault.key, maker.key, escrow.key, &[])?,
+            &[vault.clone(), maker.clone(), escrow.clone()],
+            &[&[
+                b"escrow",
+                maker.key.as_ref(),
+                escrow_data.seed.to_le_bytes().as_ref(),
+                &[bump],
+            ]],
+        )?;
+
+        // Close the escrow
+        let balance = escrow.lamports();
+        escrow.realloc(0, false)?;
+        **escrow.lamports.borrow_mut() = 0;
+        **maker.lamports.borrow_mut() += balance;
+        escrow.assign(&Pubkey::default());
+
+        Ok(())
+    }
 }
